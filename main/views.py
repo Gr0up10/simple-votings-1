7ef9d4f7a40647e918ebe4c1a97797855d8ffe0c
Grigoriy Belikov <belikovgrisha9@gmail.com>
2020-01-17 16:42:08 +0300

    Base set up

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
 from django.contrib.auth import authenticate, login
 
+from main.models import Voting, VoteVariant
+
 
 def get_menu_context():
     return [
@@ -16,8 +18,32 @@ def get_menu_context():
 
 
 def index(req, additional_context={}):
-    context = {**additional_context, 'menu': get_menu_context(), 'login_form': AuthenticationForm()}
+    context = {**additional_context, 'menu': get_menu_context(), 'login_form': AuthenticationForm(), "has_polls": False}
+    context["has_polls"] = True
+    context["polls"] = Voting.objects.all()
 
+    a = context["polls"].filter()[0]
+    a1 = VoteVariant.objects.filter()[0]
+
+    for i in context["polls"].filter():
+        vote_variants = VoteVariant.objects.filter(vote=i)
+        print(vote_variants)
+
+    return render(req, 'pages/polls_feed.html', context)
+
+    # vote = Voting(name="Котики или собачки?", description="Вопрос жизни и смерти.", finish_date=datetime.datetime.now(),
+    #               publish_date=datetime.datetime.now(),create_date=datetime.datetime.now(),author=req.user)
+    # vote.save()
+    # VoteVariant(description="Котики", vote=vote).save()
+    # VoteVariant(description="Собачки", vote=vote).save()
+    #
+    # vote = Voting(name="Зайчики или хомячки?", description="Вопрос жизни и смерти.", finish_date=datetime.datetime.now(),
+    #               publish_date=datetime.datetime.now(),create_date=datetime.datetime.now(),author=req.user)
+    # vote.save()
+    # VoteVariant(description="Хомячки", vote=vote).save()
+    # VoteVariant(description="Зайчики", vote=vote).save()
+    #
+    # print("2 votes added")
     return render(req, 'pages/polls_feed.html', context)
 
 
@@ -35,4 +61,3 @@ def login_req(request):
         return index(request, {'login_error': _('Username or password is incorrect')})
 
     return redirect('/')
-
--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login_req, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
