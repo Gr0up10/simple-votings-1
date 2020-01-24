41ae8127a34edddf5e794538895df987e26d795b
Yaroslav <maplegend@mail.ru>
2020-01-24 22:12:21 +0000

    One page app refactoring

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from urllib import parse
 import json
 
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from django.utils.translation import ugettext as _
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
@@ -16,15 +18,15 @@ from simple_votings import settings
 
 def get_menu_context():
     return [
-        {'path': '/', 'name': _('Votings')},
-        {'path': 'profile', 'name': _('Profile')},
-        {'path': 'new_voting', 'name': _('Create voting')},
-        {'path': 'logout', 'name': _('Logout')},
+        {'function': 'go_to_page(\'/\')', 'name': _('Votings')},
+        {'function': 'go_to_page(\'/profile/\')', 'name': _('Profile')},
+        {'function': 'show_voting_creation()', 'name': _('Create voting')},
+        {'function': 'go_to_page(\'/logout\')', 'name': _('Logout')},
     ]
 
 
-def index(req, additional_context={}):
-    context = {**additional_context, 'menu': get_menu_context(), 'login_form': AuthenticationForm()}
+def index(req):
+    context = {'menu': get_menu_context(), 'login_form': AuthenticationForm()}
     polls = Voting.objects.all().prefetch_related("votes")
     if polls.exists():
         context["has_polls"] = True
@@ -36,14 +38,12 @@ def index(req, additional_context={}):
 
 
 def new_voting(request):
-    contex = {'edit_voting': 'new'}
-
-    return index(request, contex)
+    return render(request, 'base/edit_voting.html')
 
 
 def login_req(request):
     if not request.POST:
-        return redirect('/')
+        return render(request, 'registration/login.html', {'login_form': AuthenticationForm()})
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
@@ -51,19 +51,20 @@ def login_req(request):
         if not request.POST.get('remember_me', None):
             request.session.set_expiry(0)
         login(request, user)
+        return JsonResponse({'success': True})
     else:
-        return index(request, {'login_error': _('Username or password is incorrect')})
-
-    return redirect('/')
+        return JsonResponse({'success': False,
+                             'error': render_to_string('registration/form_error.html',
+                                                       {'error':  _('Username or password is incorrect')})})
 
 
 def register_req(request):
-    def render_error(form, error):
-        return index(request, {'login_error': error,
-                               'registration': True, 'registration_form': form})
-
     if not request.POST:
-        return index(request, {'registration': True, 'registration_form': UserCreationForm()})
+        return render(request, 'registration/registration.html', {'registration_form': UserCreationForm()})
+
+    def render_error(form, error):
+        return JsonResponse({'success': False, 'form': render_to_string('registration/registration.html',
+                                                                        {'registration_form': form, 'error': error})})
 
     form = UserCreationForm(request.POST)
     if request.POST.get('accept_terms', None) is None:
@@ -89,6 +90,6 @@ def register_req(request):
         raw_password = form.cleaned_data.get('password1')
         user = authenticate(username=username, password=raw_password)
         login(request, user)
-        return redirect('/')
+        return JsonResponse({'success': True})
     else:
         return render_error(form, _('You filled fields incorrectly'))
